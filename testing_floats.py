9cf441c118dec9d82f60f0d1273f2ba0e662248f
williammabotjadev <williammabotjadev@gmail.com>
2022-11-08 20:52:44 +0200

    tests, testing_floats

--- testing_floats.py
+++ testing_floats.py
@@ -20,3 +20,21 @@ print(rounding_floats(3.144542524, 2))
 print(float_to_fraction(3.33))
 print(get_denominator(4, 3))
 print(get_numerator(2, 3))
+
+def test_testing_floats(module_name='this module'):
+    number1 = 1.25
+    number2 = 1
+    number3 = -1
+    number4 = 5/4
+    number6 = 6
+    assert(rounding_floats(number1, number2) == 1.2)
+    assert(rounding_floats(number1*10, number3) == 10)
+    assert(float_to_fraction(number1) == number4)
+    assert(get_denominator(number2, number6) == number6)
+    assert(get_numerator(number2, number6) == number2)
+
+    s = 'Tests in {name} have {con}!'
+    print(s.format(name=module_name, con='passed'))
+
+if __name__ == '__main__':
+    test_testing_floats()
