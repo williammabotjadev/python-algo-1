410fad980f8a1fe4490be628c3e699d7b9b1b6dd
williammabotjadev <williammabotjadev@gmail.com>
2022-11-09 18:46:07 +0200

    string formatting

---
+++ str_format.py
@@ -0,0 +1,10 @@
+# String Formatting
+
+color = "Brown"
+speed = "Quick"
+animal = "Fox"
+adjective = "Lazy"
+
+phrase = f"The {color} {speed} {animal} jumped over the {adjective} dog"
+
+print(phrase)
