a60fa0797886da742e37565639cbf29bc142391f
williammabotjadev <williammabotjadev@gmail.com>
2022-11-09 18:26:40 +0200

    deep copy, object, dict, set

--- other_deep_copy.py
+++ other_deep_copy.py
@@ -1,5 +1,8 @@
 # Making a deep copy on other collection types
 
+import copy
+
+# Deep Copy on a Set
 people = {
     "Buffy",
     "Giles",
@@ -15,3 +18,31 @@ slayers.remove("Giles")
 print(slayers)
 
 print(people)
+
+# Make a Deep copy on a Dict
+
+dict_eg = {
+    "a": 1,
+    "b": 2,
+    "c": 3,
+    "d": 4
+}
+
+newDict = dict_eg.copy()
+
+# Make a Deep copy on an Object
+
+class Student(Object):
+    def __init__(self, name, age) -> None:
+        self.name = name
+        self.age = age 
+
+
+student = Student("William", 35)
+
+newObj = copy.copy(student)
+newDeepObj = copy.deepcopy(student)
+
+print(newObj)
+print(newDeepObj)
+
