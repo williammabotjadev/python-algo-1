d3284ded944ca266b7cae636dcb8ed406864d990
williammabotjadev <williammabotjadev@gmail.com>
2022-11-09 19:38:50 +0200

    find, index, find and index methods, str

---
+++ find_index.py
@@ -0,0 +1,9 @@
+# Find and Index Method for Strings
+
+phrase = "The Brown Quick Fox jumped over the Lazy Dog"
+
+print(phrase.find("B"))
+
+limited_phrase = "The pivot was expected"
+
+print(limited_phrase.index("z"))
