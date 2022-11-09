973d76940ed185dac2747e1909fdf1ce60e73db9
williammabotjadev <williammabotjadev@gmail.com>
2022-11-09 15:09:38 +0200

    finding gcd, tests

--- finding_gcd.py
+++ finding_gcd.py
@@ -7,3 +7,12 @@ def finding_gcd(a, b):
         a, b = b, a % b 
     return result 
 
+def test():
+    x, y = 21, 12
+    m, n = 49, 7
+    assert(finding_gcd(x, y) == 3)
+    assert(finding_gcd(m, n) == 7)
+    print("Test Passed")
+
+if __name__ == "__main__":
+    test()
