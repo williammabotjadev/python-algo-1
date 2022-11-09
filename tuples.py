c9b485e8bc1be9036636ee5794da53ba3dcd8048
williammabotjadev <williammabotjadev@gmail.com>
2022-11-09 19:50:02 +0200

    tuple, empty, single item

--- tuples.py
+++ tuples.py
@@ -15,3 +15,15 @@ print(tuple_two)
 tuple_three = ("hello"), tuple_one
 
 print(tuple_three)
+
+tuple_four = 1, 
+
+empty_tuple = ()
+
+tuple_five = (1,)
+
+print(tuple_four)
+
+print(len(empty_tuple))
+
+print(tuple_five)
