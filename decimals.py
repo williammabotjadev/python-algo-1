f0c326fc950dcd790bf2dbb0e87e05b0f161e352
williammabotjadev <williammabotjadev@gmail.com>
2022-11-09 14:21:19 +0200

    Decimal, range comparison

--- decimals.py
+++ decimals.py
@@ -1,3 +1,6 @@
 # The decimal.Decimal module
+from decimal import Decimal
 
-print(sum(0.1 for i in range(10)) == 0)
+print(sum(0.1 for i in range(10)) == 1.0)
+
+print(sum(Decimal("0.1") for i in range(10)) == Decimal("1.0"))
