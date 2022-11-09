c6374c22532316000315220af27aa7dcf1cdbd47
williammabotjadev <williammabotjadev@gmail.com>
2022-11-09 15:28:09 +0200

    find fib 3 ways

--- find_fib_seq.py
+++ find_fib_seq.py
@@ -2,6 +2,8 @@
 
 import math 
 
+# Using Iteration
+
 def find_fib_iter(n):
     if n < 2:
         return n
@@ -12,7 +14,27 @@ def find_fib_iter(n):
         a, b = b, a + b
     return a
 
+# Using Recursion
+
 def find_fib_rec(n):
     if n < 2:
         return n 
     return find_fib_rec(n - 1) + find_fib_rec(n - 2)
+
+# Using a Formula 
+
+def find_fib_form(n):
+    sqrt5 = math.sqrt(5)
+    phi = (1 + sqrt5) / 2
+    return int(math.floor(phi ** n / sqrt5))
+
+
+def test():
+    n = 10
+    assert(find_fib_form(n) == 55)
+    assert(find_fib_iter(n) == 55)
+    assert(find_fib_rec(n) == 55)
+    print("Test Passed")
+
+if __name__ == "__main__":
+    test()
