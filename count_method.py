dd1dbff8a68c8231900efbc7987bbe3cfb29d786
williammabotjadev <williammabotjadev@gmail.com>
2022-11-09 19:42:24 +0200

    count method, str

---
+++ count_method.py
@@ -0,0 +1,11 @@
+# The Count method for string occurences
+
+phrase = "Buffy is Buffy is Buffy"
+
+res = phrase.count("Buffy", 0, -1)
+
+print(res)
+
+res_one = phrase.count("Buffy")
+
+print(res_one)
