3ddcb5fca6d155e1d4b54aaa2cb0d5a8abdc7708
williammabotjadev <williammabotjadev@gmail.com>
2022-11-09 17:27:06 +0200

    sequence types, bytes, bytearray, tuple, string, list

---
+++ seq_types.py
@@ -0,0 +1,16 @@
+# Basic Sequence Types
+
+l = []
+print(type(l))
+
+string_eg = "The brown quick fox jumped over the lazy dog"
+print(type(string_eg))
+
+t = (1, 3)
+print(type(t))
+
+ba = bytearray(b"")
+print(type(ba))
+
+b = bytes([])
+print(type(b))
