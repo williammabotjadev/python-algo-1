378f70b75328d4b54a757fc464eefcad1d6d6e1a
williammabotjadev <williammabotjadev@gmail.com>
2022-11-09 18:49:32 +0200

    splitlines, list res

---
+++ splitlines.py
@@ -0,0 +1,7 @@
+# Splitlines method to split lines into a list based on the position of \n
+
+phrase = "The\nBrown\nQuick\nFox\nJumped\nOver\nThe\nLazy\nDog"
+
+res = phrase.splitlines()
+
+print(res)
