9bdf759873e04b5bee529c7c49504f1b50c65174
williammabotjadev <williammabotjadev@gmail.com>
2022-11-10 13:53:07 +0200

    lambda function, creating dict keys

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,17 @@
 # Lambda Function 
 
+import collections
+
 area = lambda b, h: 0.5 * b * h
 
 print(area(5, 4))
+
+# Creating Keys in Dictionaries
+
+minus_one_dict = collections.defaultdict(lambda: -1)
+point_zero_dict = collections.defaultdict(lambda: (0,0))
+message_dict = collections.defaultdict(lambda: "No Message")
+
+print(minus_one_dict)
+print(point_zero_dict)
+print(message_dict)
