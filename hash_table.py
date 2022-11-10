de076b5bd5b54f393aade74215ebc4e97d6c0ab5
williammabotjadev <williammabotjadev@gmail.com>
2022-11-10 22:00:14 +0200

    hash table linked list, add, delete

--- hash_table.py
+++ hash_table.py
@@ -12,4 +12,21 @@ class HTLinkedList(object):
         for i in range(self.size):
             self.slots.append(LinkedListFIFO())
 
+    def _find(self, item):
+        return item % self.size 
+
+    def _add(self, item):
+        index = self._find(item)
+        self.slots[index].add_node(index)
+
+    def _delete(self, item):
+        index = self._find(item)
+        self.slots[index].delete_node(index)
+
+    
+
+    
+
+    
+
     
