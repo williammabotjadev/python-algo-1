b45efc497b741a7440fe025c55853e534459ab32
williammabotjadev <williammabotjadev@gmail.com>
2022-11-10 09:28:16 +0200

    union, intersection, basic_set

--- basic_set.py
+++ basic_set.py
@@ -4,4 +4,10 @@ people = { "Buffy", "Angel", "Giles" }
 
 print(people.add("Willow"))
 
+more_people = { "Xander", "Willow", "Giles"}
+
+print(people.intersection(more_people))
+
+print(people.union(more_people))
+
 print(people)
