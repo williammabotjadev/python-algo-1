34dcd9eeee84560ecd848213adcdf2f1d13a443a
williammabotjadev <williammabotjadev@gmail.com>
2022-11-10 09:30:09 +0200

    difference, basic_set

--- basic_set.py
+++ basic_set.py
@@ -6,8 +6,12 @@ print(people.add("Willow"))
 
 more_people = { "Xander", "Willow", "Giles"}
 
+vampires = {"Drusilia", "Angel", "Spike"}
+
 print(people.intersection(more_people))
 
 print(people.union(more_people))
+
+print(people.difference(vampires))
 
 print(people)
