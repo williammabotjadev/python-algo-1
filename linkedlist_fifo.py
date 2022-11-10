f051f18bb6c83287c1293447bb65f261632b9604
williammabotjadev <williammabotjadev@gmail.com>
2022-11-10 21:18:45 +0200

    LinkedList FIFO, __init__

---
+++ linkedlist_fifo.py
@@ -0,0 +1,11 @@
+# FIFO LinkedList 
+
+from linked_list import Node
+
+class LinkedListFIFO(object):
+    def __init__(self):
+        self.head = None 
+        self.length = 0 
+        self.tail = None 
+
+    
