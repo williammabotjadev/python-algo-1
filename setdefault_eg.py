70c393d8b3d48ee7bdc7e30c1cbea5723ee74639
williammabotjadev <williammabotjadev@gmail.com>
2022-11-10 10:31:53 +0200

    setdefault_eg, test

--- setdefault_eg.py
+++ setdefault_eg.py
@@ -15,3 +15,20 @@ def setdefault_dict(dict_data):
         new_data.setdefault(k, []).append(v)
     return new_data
 
+def test(module_name="this module"):
+    test_dict_data = (
+        ("key1", "value1"),
+        ("key1", "value2"),
+        ("key2", "value3"),
+        ("key2", "value4"),
+        ("key2", "value5")
+    )
+
+    print(usual_dict(test_dict_data))
+    print(setdefault_dict(test_dict_data))
+    name=module_name
+    con="Passed"
+    print(f"Tests in {name} in have {con}")
+
+if __name__ == "__main__":
+    test()
