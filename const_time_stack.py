5629090acc5f8a19fabbbd21c344707606cfb758
williammabotjadev <williammabotjadev@gmail.com>
2022-11-11 11:46:37 +0200

    NodeWithMind, const time stack

---
+++ const_time_stack.py
@@ -0,0 +1,9 @@
+# Using a Stack to access items at Constant Time O(1)
+
+from stack import Stack 
+
+class NodeWithMin(object):
+    def __init__(self, value=None, pointer=None):
+        self.value = value 
+        self.pointer = value 
+
