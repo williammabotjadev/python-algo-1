6a71efe9683a5865896ec1b6eadc583c831fc1d5
williammabotjadev <williammabotjadev@gmail.com>
2022-11-11 11:50:29 +0200

    StackMin, push method, const time stack

--- const_time_stack.py
+++ const_time_stack.py
@@ -7,10 +7,14 @@ class NodeWithMin(object):
         self.value = value 
         self.minimum = minimum
 
-class StackMin(object):
+class StackMin(Stack):
     def __init__(self):
         self.items = []
         self.minimum = None
 
-    
+    def push(self, value):
+        if self.isEmpty() or self.minimum > value:
+            self.minimum = value 
+        self.items.append(NodeWithMin(value, self.minimum))
 
+    
